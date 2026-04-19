d5332624366f3f64318eab3dcccd130843dd4532
alci19 <bytonalci@gmail.com>
2026-04-19 17:41:46 -0500

    Dibujamos al gato

--- cazando.js
+++ cazando.js
@@ -1,6 +1,22 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
-const ALTURA_SUELO=40;
-const ALTURA_PERSONAJE=60;
-const ANCHO_PERSONAJE=40;
-let personajeX=canvas.width/2;
+
+//tamaños
+const ALTURA_GATO=40;
+const ANCHO_GATO=60;
+
+//posiciones
+let gatoX = 0;
+let gatoY = 0;
+
+
+function graficarGato(){
+    graficarRectangulo(canvas.width/2 - ANCHO_GATO/2, canvas.height/2 - ALTURA_GATO/2, ANCHO_GATO, ALTURA_GATO);
+
+    
+}
+
+function graficarRectangulo(x, y, ancho, alto) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x, y, ancho, alto);
+}
