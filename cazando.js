be156eabb3651130913fcbd0c213dc6a5c389f03
alci19 <bytonalci@gmail.com>
2026-04-19 08:54:58 -0500

    Agregamos los recursos del juego

---
+++ cazando.js
@@ -0,0 +1,6 @@
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+const ALTURA_SUELO=40;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
