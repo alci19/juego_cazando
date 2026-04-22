c7be8087072c5885c77ddf825b821b0296382c55
alci19 <bytonalci@gmail.com>
2026-04-22 14:09:06 -0500

    Añadimon funcion de mover a la izquierda y limpiar canvas

--- cazando.js
+++ cazando.js
@@ -19,7 +19,7 @@ function iniciarJuego(){
 }
 
 function graficarGato(){
-    graficarRectangulo(canvas.width/2 - ANCHO_GATO/2, canvas.height/2 - ALTURA_GATO/2, ANCHO_GATO, ALTURA_GATO, "blue");
+    graficarRectangulo(canvas.width/2 - ANCHO_GATO/2 + gatoX, canvas.height/2 - ALTURA_GATO/2 - gatoY, ANCHO_GATO, ALTURA_GATO, "blue");
 
     
 }
@@ -32,3 +32,18 @@ function graficarRectangulo(x, y, ancho, alto, color) {
 function graficarComida(){
     graficarRectangulo(canvas.width - ANCHO_COMIDA, canvas.height - ALTURA_COMIDA, ANCHO_COMIDA, ALTURA_COMIDA, "orange");
 }
+
+function limpiarCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function moverIzquierda(){
+    gatoX-=10;
+    actualizarPantalla();
+}
+function actualizarPantalla(){
+    limpiarCanvas();
+    graficarGato();
+    graficarComida();
+}
+
